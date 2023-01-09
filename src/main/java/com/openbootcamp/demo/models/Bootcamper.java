0dbad1074a67f5cf94753782411ac90b33f44c76
Luis <luiszelarrayan@hotmail.com>
2023-01-09 14:04:12 -0300

    Api Rest con java

---
+++ src/main/java/com/openbootcamp/demo/models/Bootcamper.java
@@ -0,0 +1,32 @@
+package com.openbootcamp.demo.models;
+
+// POJO : Plain old java object : modelo de datos
+
+public class Bootcamper {
+    private String nombre;
+    private double valor;
+
+    public Bootcamper() {
+    }
+
+    public Bootcamper(String nombre, double valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
